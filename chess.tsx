77181d50009910bda61f83174dee6ad4a002fedf
ihalseide <imitatio-ignotum@protonmail.com>
2021-01-24 21:51:15 -0700

    Add tilemap data for backgrounds

---
+++ chess.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="chess" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="spritesheet.png" width="256" height="256"/>
+</tileset>
