578c18fb290fca55ce8e7d7c2b95918bc4250beb
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-28 16:22:48 -0700

    Update graphics and tilemaps

--- resources/chess.tsx
+++ resources/chess.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="chess" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
- <image source="../spritesheet.png" width="256" height="256"/>
+<tileset version="1.4" tiledversion="1.4.3" name="chess" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../textures.png" width="256" height="256"/>
 </tileset>
